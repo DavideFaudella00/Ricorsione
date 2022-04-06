e20693b7289a731657a94fd1c11a1c99baa0fc06
david <david@Razer15.lan>
2022-04-06 21:45:35 +0200

    Finito

---
+++ src/main/java/it/polito/tdp/ricorsione/model/TestQuadrato.java
@@ -0,0 +1,10 @@
+package it.polito.tdp.ricorsione.model;
+
+public class TestQuadrato {
+
+	public static void main(String[] args) {
+		QuadratoMagico q = new QuadratoMagico();
+		q.risolviQuadrato(3);
+	}
+
+}
